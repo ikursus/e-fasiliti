083854ccd5e7b180f2df1d74bd0acddf22285749
anwar77 <papazara2012@gmail.com>
2026-09-10 11:50:37 +0800

    chore(m09): gd extension dalam pelarian ujian

--- storage/run_tests.cmd
+++ storage/run_tests.cmd
@@ -4,5 +4,5 @@ set PHP=C:\laragon\bin\php\php-8.4.25-nts-Win32-vs17-x64\php.exe
 set EXT=C:\laragon\bin\php\php-8.4.25-nts-Win32-vs17-x64\ext
 set GIT=C:\laragon\bin\git\cmd
 path %GIT%;%PATH%
-"%PHP%" -d extension_dir="%EXT%" -d extension=mbstring -d extension=openssl -d extension=pdo_mysql -d extension=pdo_sqlite -d extension=sqlite3 -d extension=fileinfo -d extension=curl vendor\phpunit\phpunit\phpunit %* > storage\run_out.txt 2>&1
+"%PHP%" -d extension_dir="%EXT%" -d extension=mbstring -d extension=openssl -d extension=pdo_mysql -d extension=pdo_sqlite -d extension=sqlite3 -d extension=fileinfo -d extension=curl -d extension=gd vendor\phpunit\phpunit\phpunit %* > storage\run_out.txt 2>&1
 echo EXIT=%ERRORLEVEL% >> storage\run_out.txt
